b9af4a6ad42ccd25bc91c8bc389b7a85cbf50199
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-15 15:12:41 -0300

    Finalizado a PageLoading e suas animalções

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import PageLoaading from './components/PageLoading.vue';
 
 Vue.config.productionTip = false;
+
+Vue.component('PageLoading', PageLoaading);
 
 new Vue({
   router,
