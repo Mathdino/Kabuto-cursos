1f193db924f2ee885a0eeb035b695afd07ef8385
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-16 13:07:22 -0300

    Finalizado o projeto

--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <div v-if="loading">
-      <PageLoading/>
+      <PageLoading />
     </div>
     <transition>
       <div v-if="api" class="conteudo">
         <div>
-          <h1>{{api.titulo}}</h1>
-          <p>{{api.descricao}}</p>
+          <h1>{{ api.titulo }}</h1>
+          <p>{{ api.descricao }}</p>
         </div>
-        <ul>
+        <ul class="cursos-lista">
           <li v-for="curso in api.cursos" :key="curso.id">
             <h2>
-              <router-link
-                :to="{name: 'curso', params: {curso: curso.id}}"
-              >{{curso.nome}} – {{curso.totalAulas}} aulas | {{curso.horas}} horas</router-link>
+              <router-link :to="{ name: 'curso', params: { curso: curso.id } }"
+                >{{ curso.nome }} – {{ curso.totalAulas }} aulas |
+                {{ curso.horas }} horas</router-link
+              >
             </h2>
-            <p>{{curso.descricao}}</p>
+            <p>{{ curso.descricao }}</p>
           </li>
         </ul>
       </div>
@@ -25,13 +26,19 @@
 </template>
 
 <script>
-import fetchData from "@/mixins/fetchData.js";
+import fetchData from '@/mixins/fetchData.js';
 
 export default {
-  name: "CursosView",
+  name: 'CursosView',
   mixins: [fetchData],
   created() {
-    this.fetchData("/cursos");
-  }
+    this.fetchData('/cursos');
+  },
 };
 </script>
+
+<style>
+.cursos-lista li {
+  margin-bottom: 40px;
+}
+</style>
